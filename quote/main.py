efd0daa227dee2e5a2d20b3cefe2bca245e8b0b5
vahid <vahid@kharazi.net>
2013-11-08 20:33:18 +0330

    Create static and templates folder

--- quote/main.py
+++ quote/main.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
 quote = Flask(__name__)
 
 @quote.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html', name="quote")
+
+
+@quote.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     quote.debug = True
